5cbe8086f77872ef5243029c410e174e070756d0
taeyang916 <76814748+taeyang916@users.noreply.github.com>
2024-02-12 18:20:10 +0900

    Feat : Add utils.py
    - #2

---
+++ utils.py
@@ -0,0 +1,86 @@
+
+import os
+import cv2
+import torch
+import random
+import numpy as np
+import PIL as plt
+
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+PALETTE = [
+    (220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
+    (0, 60, 100), (0, 80, 100), (0, 0, 70), (0, 0, 192), (250, 170, 30),
+    (100, 170, 30), (220, 220, 0), (175, 116, 175), (250, 0, 30), (165, 42, 42),
+    (255, 77, 255), (0, 226, 252), (182, 182, 255), (0, 82, 0), (120, 166, 157),
+    (110, 76, 0), (174, 57, 255), (199, 100, 0), (72, 0, 118), (255, 179, 240),
+    (0, 125, 92), (209, 0, 151), (188, 208, 182), (0, 220, 176),
+]
+
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = y_true.flatten(2)
+    y_pred_f = y_pred.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
+    
+    eps = 0.0001
+    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps)
+
+def save_model(model, file_name='fcn_resnet50_best_model.pt', model_dir="models"):
+    if not os.path.exists(model_dir):                                                           
+        os.makedirs(model_dir)
+    output_path = os.path.join(model_dir, file_name)
+    torch.save(model, output_path)
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+def label2rgb(label):
+    image_size = label.shape[1:] + (3, )
+    image = np.zeros(image_size, dtype=np.uint8)
+    
+    for i, class_label in enumerate(label):
+        image[class_label == 1] = PALETTE[i]
+        
+    return image
+
+def decode_rle_to_mask(rle, height, width):
+    s = rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(height * width, dtype=np.uint8)
+    
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    
+    return img.reshape(height, width)
+
+def save_image(data_path, saved_path, rles, filename_and_class):
+    image = cv2.imread(os.path.join(data_path, filename_and_class[0].split("_")[1]))
+    preds = []
+    for rle in rles[:len(CLASSES)]:
+        pred = decode_rle_to_mask(rle, height=2048, width=2048)
+        preds.append(pred)
+
+    preds = np.stack(preds, 0)
+
+    fig, ax = plt.subplots(1, 2, figsize=(24, 12))
+    ax[0].imshow(image)
+    ax[1].imshow(label2rgb(preds))
+    cv2.imwrite(os.path.join(saved_path, 'inferenced_image'), ax)
