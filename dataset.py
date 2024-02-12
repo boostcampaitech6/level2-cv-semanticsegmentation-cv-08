0beb5295bd9033aa19aff1c01b1766e1401a9ff2
taeyang916 <76814748+taeyang916@users.noreply.github.com>
2024-02-12 17:54:28 +0900

    Feat : Modified dataset.py
    - #2

--- dataset.py
+++ dataset.py
@@ -1,28 +1,15 @@
 # python native
 import os
 import json
-import random
-import datetime
-from functools import partial
 
 # external library
 import cv2
 import numpy as np
-import pandas as pd
-from tqdm.auto import tqdm
 from sklearn.model_selection import GroupKFold
-import albumentations as A
 
 # torch
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from torchvision import models
-
-# visualization
-import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
 
 CLASSES = [
     'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
